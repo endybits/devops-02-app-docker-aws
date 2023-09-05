b28e2beeda2d7af2cc47a60e9d9f5dff23a647f7
root <root@ENDY-BERMUDEZ>
2023-09-05 15:37:25 +1000

    Create hello fastapi app

---
+++ app/main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def hello_devops():
+    return {
+        'msg': "Hello, this is an Application Running on AWS",
+        'project': "devops-02-app-docker-aws",
+        'devops_engineer': "Endy B!"
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app)
